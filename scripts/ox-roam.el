9b8141ac4364a1e697a180a3e37d52ef8a9900c7
Paw Møller <pawsen@gmail.com>
2024-10-22 21:00:40 +0200

    setup org-attach

--- scripts/ox-roam.el
+++ scripts/ox-roam.el
@@ -8,6 +8,7 @@
 
 (require 'ox-hugo)
 (require 'org-roam)
+
 (defun collect-backlinks-string (backend)
   (when (org-roam-node-at-point)
     (goto-char (point-max))
@@ -26,16 +27,45 @@
 
 (add-hook 'org-export-before-processing-functions #'collect-backlinks-string)
 
+(defun org-hugo--tag-processing-fn-remove-tags-maybe (tags-list info)
+  "Remove user-specified tags/categories.
+See `org-hugo-tag-processing-functions' for more info."
+  ;; Use tag/category string (including @ prefix) exactly as used in Org file.
+  (let ((tags-categories-to-be-removed '("DONE" "ATTACH"))) ;"my_tag" "@my_cat"
+    (cl-remove-if (lambda (tag_or_cat)
+                    (member tag_or_cat tags-categories-to-be-removed))
+                  tags-list)))
+(add-to-list 'org-hugo-tag-processing-functions
+             #'org-hugo--tag-processing-fn-remove-tags-maybe)
+
+
 (defun export-org-roam-files ()
   "Exports Org-Roam files to Hugo markdown."
   (interactive)
 
+  ;; extend the list of file extentions that gets copied to the public/ox-hugo dir
+  ;; default is
+  ;; ("jpg" "jpeg" "tiff" "png" "svg" "gif" "bmp" "mp4" "pdf" "odt" "doc" "ppt" "xls"
+  ;; "docx" "pptx" "xlsx")
   (setq org-hugo-external-file-extensions-allowed-for-copying
         (append org-hugo-external-file-extensions-allowed-for-copying
                 '("wav" "raw" "epub")))
 
-  (print org-hugo-external-file-extensions-allowed-for-copying)
- 
+  (message "org-hugo-external-file-extensions-allowed-for-copying are '%s'"
+           org-hugo-external-file-extensions-allowed-for-copying)
+
+  ;; "Sets up org's attachment system."
+  ;; see doom emacs org-attch initialization
+  (setq
+   org-attach-store-link-p 'attached     ; store link after attaching files
+   org-attach-use-inheritance t ; inherit properties from parent nodes
+   org-attach-id-dir (expand-file-name ".attach/" default-directory))
+  ;; no need to set the org-directory
+  ;; (setq-default org-attach-id-dir (expand-file-name ".attach/" org-directory))
+
+  (message "default-directory is '%s'" default-directory)
+  (message "org-attach-id-dir set to '%s'" org-attach-id-dir)
+
   (let ((org-id-extra-files (directory-files-recursively default-directory "notes")))
     (dolist (f (append (file-expand-wildcards "org/about.org")
                        (file-expand-wildcards "org/diary/*.org")
