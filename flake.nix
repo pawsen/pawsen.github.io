052bdf32def48cc192ba73ec0f953c9a4592e3e0
Paw Møller <pawsen@gmail.com>
2024-11-19 19:01:01 +0100

    feat: solely use flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,28 @@
+# use with
+# nix develop -c $SHELL
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
   outputs = { nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
-      });
+    let forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in with pkgs; rec {
+          default = mkShell {
+            # Put shell variables here
+
+            buildInputs = with pkgs; [
+              hugo
+              dart-sass
+              # go  # for hugo modules
+            ];
+
+          };
+        });
       packages = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
+        default =
+          nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
       });
     };
 }
-
