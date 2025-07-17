16c1b51d0036cc5bb07f59b282ed2b32da9f1204
Paw Møller <pawsen@gmail.com>
2025-07-17 21:19:52 +0200

    feat: copy images to bundle folder

--- scripts/ox-roam.el
+++ scripts/ox-roam.el
@@ -39,6 +39,59 @@ See `org-hugo-tag-processing-functions' for more info."
              #'org-hugo--tag-processing-fn-remove-tags-maybe)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; copy images to the hugo bundle dir
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; List the images to copy in the properties drawer using link-img key.
+;; :PROPERTIES:
+;; :link-img:  some/folder/some*.jpg
+;; :link-img:  some/folder/other*.jpg
+;; :END:
+(defvar my/ox-hugo-copy-verbose t
+  "When non-nil, print diagnostics while copying :link-img: files.")
+
+(defun my/ox-hugo--all-link-img-values (&optional subtree-only)
+  "Return a list of all :link-img: property values in current subtree."
+  (let* ((parse (if subtree-only
+                    (org-element-parse-buffer 'element)
+                  (org-element-parse-buffer)))
+         (vals
+          (org-element-map parse 'node-property
+            (lambda (np)
+              (when (string-equal (downcase (org-element-property :key np))
+                                  "link-img")
+                (string-trim (org-element-property :value np))))
+            nil nil t)))
+    (when my/ox-hugo-copy-verbose
+      (message "[ox-hugo] link-img patterns: %S" vals))
+    vals))
+
+(defun my/ox-hugo-copy-link-img (outfile)
+  "Copy all wildcards in :link-img: into the bundle dir of OUTFILE."
+  (when (and outfile (file-exists-p outfile))
+    (let* ((bundle (file-name-directory outfile))
+           (base   (file-name-directory (buffer-file-name))))
+      (dolist (pat (my/ox-hugo--all-link-img-values))
+        (let* ((abs   (expand-file-name pat base))
+               (files (file-expand-wildcards abs t)))
+          (when my/ox-hugo-copy-verbose
+            (message "[ox-hugo] %s ⇒ %d match(s)" abs (length files)))
+          (dolist (src files)
+            (let ((dest (expand-file-name (file-name-nondirectory src) bundle)))
+              (when my/ox-hugo-copy-verbose
+                (message "[ox-hugo] copy %s → %s" src dest))
+              (copy-file src dest :ok-if-already-exists t))))))))
+
+(defun my/ox-hugo-copy-wim-advice (orig &rest args)
+  "Around-advice: run ORIG, then copy :link-img: files."
+  (let ((outfile (apply orig args)))  ; run the real exporter
+    (my/ox-hugo-copy-link-img outfile)
+    outfile))
+
+(with-eval-after-load 'ox-hugo
+  (advice-add 'org-hugo-export-wim-to-md :around
+              #'my/ox-hugo-copy-wim-advice))
+
 (defun export-org-roam-files ()
   "Exports Org-Roam files to Hugo markdown."
   (interactive)
